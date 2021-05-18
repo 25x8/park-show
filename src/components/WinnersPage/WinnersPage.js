6fdbe147e4fb308f13928838b09baf1e2b8735de
Shovkin <artishock4live@gmail.com>
2021-05-18 17:41:05 +0300

    disabled click on carousel

--- src/components/WinnersPage/WinnersPage.js
+++ src/components/WinnersPage/WinnersPage.js
@@ -23,7 +23,7 @@ const WinnersPageBase = ({className}) => {
                     <div
                         className="section__title"
                         onClick={() => {
-                            dispatch(changeAfkState(true));
+                            // dispatch(changeAfkState(true));
                         }}
                     >
                         Конкурсы</div>
